9f4a8c2a0a2c0ae783b621e4f3d0f4ced9ee2928
João Vitor Duarte Coelho <91630610+victorsoot@users.noreply.github.com>
2022-05-05 12:07:34 -0300

    final update

---
+++ comp2.py
@@ -0,0 +1,14 @@
+import os
+os.system("clear")
+os.system("git clone https://github.com/cutefishos/filemanager --branch=0.3 ~/Downloads/cutefish/filemanager")
+os.system("cd ~/Downloads/cutefish/filemanager && dpkg-buildpackage -uc -us -b")
+os.system("git clone https://github.com/cutefishos/settings --branch=0.3 ~/Downloads/cutefish/settings")
+os.system("cd ~/Downloads/cutefish/settings && dpkg-buildpackage -uc -us -b")
+os.system("git clone https://github.com/cutefishos/statusbar --branch=0.3 ~/Downloads/cutefish/statusbar")
+os.system("cd ~/Downloads/cutefish/statusbar && dpkg-buildpackage -uc -us -b")
+os.system("git clone https://github.com/cutefishos/core --branch=0.3 ~/Downloads/cutefish/core")
+os.system("cd ~/Downloads/cutefish/core && dpkg-buildpackage -uc -us -b")
+os.system("cd ~/Downloads/cutefish && sudo apt-get install -y ./*.deb")
+os.system("git clone https://github.com/cutefishos/calculator --branch=0.3 ~/Downloads/cutefish/calculator && cd ~/Downloads/cutefish/calculator && mkdir build && cd build/ && cmake .. && make && sudo make install")
+os.system("git clone https://github.com/cutefishos/icons --branch=0.3 ~/Downloads/cutefish/icons && cd icons && mkdir build && cd build && cmake .. && make && sudo make install")
+#end
